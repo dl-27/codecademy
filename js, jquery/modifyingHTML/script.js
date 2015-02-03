40e9eae4575b59907990a65af4461007c2c031e8
Daria <da6a.93@gmail.com>
2015-02-03 18:10:33 +0200

    new jquery

---
+++ js, jquery/modifyingHTML/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#text').addClass('highlighted');
+    });
+
+/* other variants:
+$(document).ready(function(){
+    $('#text').toggleClass('highlighted');
+    });
+
+$(document).ready(function(){
+    $("div").height("200px");
+    $("div").width("200px");
+    $("div").css("border-radius", "10px");
+    });
+
+$(document).ready(function(){
+    $("p").html("some text");
+    });
+*/
